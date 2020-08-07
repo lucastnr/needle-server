f6854b96c4007e52350fc0f4963db6d1df829366
Lucas <tnrlucas@gmail.com>
2020-08-06 23:56:30 -0300

    version 0.0.1

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const server = express()
+const port = 3000
+
+const data = require('./data.json')
+
+server.get('/', (req, res) => {
+    console.log(' GET / ')
+    res.json(data)
+}) 
+
+server.use(express.static('../public'))
+server.listen(port, () => console.log(`Server ligado na porta ${port}!`))
