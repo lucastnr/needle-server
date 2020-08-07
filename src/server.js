9e30da983ae0e2f035468d03dc0c23e98826bb66
Lucas <tnrlucas@gmail.com>
2020-08-07 03:23:19 -0300

    version 0.0.1

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const server = express()
-const port = 3000
+const port = 5000
 const data = require('./data.json')
 
 server.get('/', (req, res) => {
     console.log(' GET / ')
     res.json(data)
-}) 
+})
+
+server.get('/login/:email/:password', (req, res) => {
+    const { email, password } = req.params
+    const client = data.find(cli =>
+        cli.email == email && cli.password == password
+    )
+
+    res.json(client)
+})
 
 server.use(express.static('../public'))
-server.listen(process.env.PORT || 5000, () => console.log(`Server ligado na porta ${port}!`))
+server.listen(process.env.PORT || port, () => console.log(`Server ligado na porta ${port}!`))
